58b7cb160a4a3a569da02228fc91837f66f98be9
Diana <wwwavvves@gmail.com>
2024-07-18 15:37:30 +0100

    Added styling

--- src/Search.js
+++ src/Search.js
@@ -4,12 +4,10 @@ import "./Search.css";
 export default function Search() {
   return (
     <div className="Search">
-      <div className="row">
         <form>
-          <input type="search" placeholder="Enter a city" className="col-9" />
-          <input type="submit" value="Search" className="col-3" />
+          <input type="search" placeholder="Enter a city" className="search-input" autofocus="on" />
+          <input type="submit" value="Search" className="search-btn" />
         </form>
-      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -4,16 +4,21 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div className="row">
-        <div className="col-6">
+      <div className="row align-items-center">
+        <div className="col-6 weather-details">
           <h1>Lisbon</h1>
           <ul>
             <li>Tuesday 17:50, clear sky</li>
-            <li>Humidity: 56%, Wind: 10.29km/h</li>
+            <li>
+              Humidity: <span className="humidity">56%</span>, Wind:{" "}
+              <span className="wind">10.29km/h</span>
+            </li>
           </ul>
         </div>
-        <div className="col-6">
-            24ºC
+        <div className="col-6 city-temperature d-flex justify-content-end">
+          <div className="current-weather-icon">☀️</div>
+          <div className="current-temperature">24</div>
+          <div className="celsius">ºC</div>
         </div>
       </div>
     </div>
