ab44b1cca36fef9f8b349bc6847aebe8581a28b7
Diana <wwwavvves@gmail.com>
2024-07-18 11:27:02 +0100

    Deleted footer

--- src/App.js
+++ src/App.js
@@ -4,20 +4,6 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <footer>
-        Coded by{" "}
-        <a href="https://github.com/wwwavvves" traget="_blank" rel="noreferrer">
-          Diana Guerreiro
-        </a>{" "}
-        and open-sourced on{" "}
-        <a
-          href="https://github.com/wwwavvves/weather-app-react-shecodes-week5"
-          traget="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
     </div>
   );
 }
