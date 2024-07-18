675eb45b907cabb8836f3f84d975e618b1ab4452
Diana <wwwavvves@gmail.com>
2024-07-18 11:35:08 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <footer>
+        Coded by{" "}
+        <a href="https://github.com/wwwavvves" target="_blank" rel="noreferrer">
+          Diana Guerreiro
+        </a>{" "}
+        and open-sourced at{" "}
+        <a
+          href="https://github.com/wwwavvves/weather-app-react-shecodes-week5"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
